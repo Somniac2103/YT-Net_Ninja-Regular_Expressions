c10d65f82a996c77e321202021e4ee15d0a4e2e4
Somniac2103 <salmon.barry1@gmail.com>
2023-09-13 15:45:08 +0800

    Completed Course!

---
+++ validation.js
@@ -0,0 +1,30 @@
+// Two different syntax
+//let reg = /[a-z]/gi;
+//let reg2 = new RegExp(/[a-z]/, 'g');
+
+const input = document.querySelectorAll('input');
+
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8,20}$/i,
+  slug: /^[a-z\d-]{8,20}$/i,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+};
+
+//validation function
+function validate(field, regex){
+  //console.log(regex.test(field.value))
+  if(regex.test(field.value)){
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid';
+  }
+}
+
+input.forEach((input) => {
+  input.addEventListener('keyup',(e) =>{
+    //console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  })
+})
